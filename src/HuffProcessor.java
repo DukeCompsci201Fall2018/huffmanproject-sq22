edae2f6eb880ecadfdd134ed3403bafa4a30bb10
selenaqian <selenaq2010@gmail.com>
2018-12-04 00:50:29 -0500

    decompress v1

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -58,11 +58,68 @@ public class HuffProcessor {
 	 *            Buffered bit stream writing to the output file.
 	 */
 	public void decompress(BitInputStream in, BitOutputStream out){
-
-		while (true){
+		
+		int bits = in.readBits(BITS_PER_INT);
+		if (bits != HUFF_TREE) {
+			throw new HuffException("illegal header starts with " + bits);
+		}
+		if (bits == -1) {
+			throw new HuffException("reading bits failed");
+		}
+		
+		HuffNode root = readTreeHeader(in); //readTreeHeader should read tree to decompress
+		readCompressedBits(root, in, out); //readCompressedBits should read from
+		//compressed file and traverse paths of root and write leaf values to output file
+		out.close();
+		
+/**		while (true){
 			int val = in.readBits(BITS_PER_WORD);
 			if (val == -1) break;
 			out.writeBits(BITS_PER_WORD, val);
+		} */
+	}
+	
+	/**
+	 *TODO: write comments here
+	 * @param in
+	 * @return
+	 */
+	public HuffNode readTreeHeader(BitInputStream in) {
+		int oneBit = in.readBits(1);
+		if (oneBit == -1) {
+			throw new HuffException("reading bits failed");
+		}
+		if (oneBit == 0) { //bc if equals 1 then finished
+			HuffNode left = readTreeHeader(in);
+			HuffNode right = readTreeHeader(in);
+			return new HuffNode(0,0,left,right);
+		}
+		else {
+			int value = in.readBits(9);
+			return new HuffNode(value,0,null, null);
+		}
+	}
+	
+	
+	public void readCompressedBits(HuffNode root, BitInputStream in, BitOutputStream out) {
+		HuffNode current = root;
+		while (true) {
+			int oneBit = in.readBits(1);
+			if (oneBit == -1) {
+				throw new HuffException("bad input, no PSEUDO_EOF");
+			}
+			else {
+				if (oneBit == 0) current = current.myLeft;
+				else current = current.myRight;
+				
+				if (current.myLeft == null && current.myRight == null) {
+					if (current.myValue == PSEUDO_EOF) break;
+					else {
+						out.write(BITS_PER_WORD);
+						current = root;
+					}
+				}
+			}
 		}
 	}
 }
